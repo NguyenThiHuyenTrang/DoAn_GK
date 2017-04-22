d030fd00f9c44e9e85534e328fb35728bd344d62
DESKTOP-E7DO0MP\Huyền Trang <nthtrang2901@gmail.com>
2017-04-22 23:15:53 +0700

    Add Database

---
+++ DAGK/DAGK/Controllers/SHOPController.cs
@@ -0,0 +1,92 @@
+﻿using DAGK.Models.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAGK.Controllers
+{
+    public class SHOPController : Controller
+    {
+        // GET: SHOP
+        public ActionResult Index()
+        {
+            var dsSanPham = SHOPBus.DanhSach();
+            return View(dsSanPham);
+
+        }
+
+        // GET: SHOP/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(SHOPBus.ChiTiet(id));
+        }
+
+        // GET: SHOP/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SHOP/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SHOP/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SHOP/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SHOP/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SHOP/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DAGK/DAGK/Models/Bus/SHOPBus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WEB2Connection;
+
+namespace DAGK.Models.Bus
+{
+    public class SHOPBus
+    {
+        public static IEnumerable<Product> DanhSach()
+        {
+            var db = new WEB2ConnectionDB();
+            return db.Query<Product>("select  * from Product");
+        }
+        public static Product ChiTiet(int id)
+        {
+            var db = new WEB2ConnectionDB();
+            return db.SingleOrDefault<Product>("select * from Product where ProductID = @0", id);
+        }
+    }
+}
